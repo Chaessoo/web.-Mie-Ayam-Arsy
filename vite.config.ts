480724b623247738ea112a5c21caace3f8c9791b
Usamah Hasan Zaid <hasanzusamah@gmail.com>
2026-02-08 02:10:15 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/web.-Mie-Ayam-Arsy/' // NAMA REPO PERSIS
+})
+
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
